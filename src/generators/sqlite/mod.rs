606ca20fb0cce1b06d7b4aa966ded1d60aaab0ee
cyw0ng95 <chunyu.wang.1995@connect.hku.hk>
2025-06-02 11:39:27 +0000

    generator: update get by seed interface
    Signed-off-by: cyw0ng95 <chunyu.wang.1995@connect.hku.hk>

--- src/generators/sqlite/mod.rs
+++ src/generators/sqlite/mod.rs
@@ -1,2 +1,14 @@
 pub mod select_stmt;
 pub mod schema;
+
+use rusqlite::Connection;
+
+pub enum SQL_KIND {
+    SELECT,
+}
+
+pub fn get_stmt_by_seed(sqlite_conn: &Connection, seed: i64, kind: SQL_KIND) -> Option<String> {
+    match kind {
+        SQL_KIND::SELECT => select_stmt::get_stmt_by_seed(sqlite_conn, seed),
+    }
+}
