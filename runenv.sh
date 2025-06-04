6643e086eea682ce58ad68914d13f1ead227f960
cyw0ng95 <chunyu.wang.1995@connect.hku.hk>
2025-06-05 00:25:07 +0800

    runenv: improve db connection
    Signed-off-by: cyw0ng95 <chunyu.wang.1995@connect.hku.hk>

--- runenv.sh
+++ runenv.sh
@@ -9,18 +9,30 @@ pull_mariadb_image() {
 run_mariadb_container() {
     CURRENT_DIR=$(pwd)
 
+    # Read parameters from runtime.json
+    ROOT_PASSWORD=$(jq -r '.mariadb.rootPassword' "$CURRENT_DIR/runtime.json")
+    DATABASE=$(jq -r '.mariadb.database' "$CURRENT_DIR/runtime.json")
+    HOST_PORT=$(jq -r '.mariadb.hostPort' "$CURRENT_DIR/runtime.json")
+    CONTAINER_PORT=$(jq -r '.mariadb.containerPort' "$CURRENT_DIR/runtime.json")
+
     # Create the .runtime and .runtime/mariadb directories
     mkdir -p "$CURRENT_DIR/.runtime/mariadb"
 
     podman run -d \
         --name sqlsmith-rs_mariadb \
-        -e MYSQL_ROOT_PASSWORD=rootpassword \
-        -e MYSQL_DATABASE=fuzzerdb \
+        -e MYSQL_ROOT_PASSWORD="$ROOT_PASSWORD" \
+        -e MYSQL_DATABASE="$DATABASE" \
         -v "$CURRENT_DIR/.runtime/mariadb:/var/lib/mysql" \
-        -p 13306:3306 \
+        -p "$HOST_PORT:$CONTAINER_PORT" \
+        --replace \
         docker.io/library/mariadb:latest
 
-    echo "MariaDB container started with volume mount. Data is saved in $CURRENT_DIR/.runtime/mariadb. You can now connect to it at localhost:3306 with root password 'rootpassword' and database 'fuzzerdb'."
+    echo "MariaDB container started with volume mount. Data is saved in $CURRENT_DIR/.runtime/mariadb. You can now connect to it at localhost:$HOST_PORT with root password '$ROOT_PASSWORD' and database '$DATABASE'."
+}
+
+# Function to show container status
+show_container_status() {
+    podman ps -a --filter "name=sqlsmith-rs_mariadb"
 }
 
 # Function to show help
@@ -29,6 +41,7 @@ show_help() {
     echo "Options:"
     echo "  -p  Pull the MariaDB container image."
     echo "  -r  Run the MariaDB container."
+    echo "  -l  Show the current status of the MariaDB container."
     echo "  -h  Show this help message."
 }
 
@@ -39,13 +52,16 @@ if [ $# -eq 0 ]; then
 fi
 
 # Parse command line arguments
-while getopts "prh" opt; do
+while getopts "prlh" opt; do
     case $opt in
         p)
             pull_mariadb_image
             ;;
         r)
             run_mariadb_container
+            ;;
+        l)
+            show_container_status
             ;;
         h)
             show_help
