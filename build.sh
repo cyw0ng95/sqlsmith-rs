9213ee2c3a803642689c39ed89b525432d276b4c
cyw0ng95 <chunyu.wang.1995@connect.hku.hk>
2025-06-11 01:18:07 +0800

    bugfix: fix build.sh command
    Signed-off-by: cyw0ng95 <chunyu.wang.1995@connect.hku.hk>

--- build.sh
+++ build.sh
@@ -1,15 +1,42 @@
 #!/bin/bash
 
-if command -v sccache &> /dev/null
-then
+RUN=0
+show_help() {
+    echo "Usage: $0 [-r] [-h]"
+    echo "  -r: Run the program after building and testing."
+    echo "  -h: Show this help message."
+    exit 0
+}
+
+while getopts "rh" opt; do
+    case $opt in
+        r)
+            RUN=1
+            ;;
+        h)
+            show_help
+            ;;
+        *)
+            show_help
+            ;;
+    esac
+done
+
+if command -v sccache &> /dev/null; then
     export RUSTC_WRAPPER=sccache
 fi
 
-cd view && {
-    pnpm install && \
+if [ $RUN -eq 1 ]; then
+    cd view && {
+        pnpm install
         pnpm run dev &
-} && cd ~-
+    }
+    cd ~-
+fi
 
 cargo build && \
-    cargo test && \
+    cargo test 
+
+if [ $RUN -eq 1 ]; then
     cargo run --bin server
+fi
