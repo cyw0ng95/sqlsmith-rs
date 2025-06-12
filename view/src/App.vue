23e76893daa0c7d1295c4dff9a64ad948873653f
cyw0ng95 <chunyu.wang.1995@connect.hku.hk>
2025-06-13 01:04:33 +0800

    refactor: Restructure App.vue layout to improve component organization and styling
    Signed-off-by: cyw0ng95 <chunyu.wang.1995@connect.hku.hk>

--- view/src/App.vue
+++ view/src/App.vue
@@ -7,8 +7,12 @@ import StatsWatch from './components/StatsWatch'
 <template>
   <div id="app">
     <main class="main-flex">
-      <ProfileForm />
-      <StatsWatch />
+      <div class="left-panel">
+        <ProfileForm />
+      </div>
+      <div class="right-panel">
+        <StatsWatch />
+      </div>
     </main>
   </div>
 </template>
@@ -19,7 +23,19 @@ import StatsWatch from './components/StatsWatch'
   flex-direction: row;
   gap: 32px;
   align-items: flex-start;
-  justify-content: center;
-  min-height: 80vh;
+  justify-content: flex-start;
+  min-height: 100vh;
+  padding: 20px;
+}
+
+.left-panel {
+  flex: 1;
+  min-width: 400px;
+  max-width: 600px;
+}
+
+.right-panel {
+  flex: 1;
+  min-width: 400px;
 }
 </style>
